105483c4e0570f7ab26e53d1d1ed131f6082e080
Ethan Yao <ey709@nyu.edu>
2020-02-16 18:09:43 -0500

    added texture loader

--- P1/Project 1/main.cpp
+++ P1/Project 1/main.cpp
@@ -11,12 +11,37 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
 ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix;
 
+GLuint LoadTexture(const char* filePath)
+{
+	int w, h, n;
+	unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+	if (image == NULL)
+	{
+		cout << "Unable to load image." << endl;
+		assert(false);
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA， GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	stbi_image_free(image);
+	return textureID;
+}
 
 int main(int argc, char* argv[])
 {
@@ -28,6 +53,10 @@ int main(int argc, char* argv[])
 #ifdef _WINDOWS
 	glewInit();
 #endif
+
+	program.Load("shaders/vertex_textured.glsl", "shaders/fragment_textured.glsl");
+
+	playerTextureID = LoadTexture("ctg.png");
 
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 
