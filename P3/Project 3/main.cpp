7e431093a71736381995154b085322fc51dbdac3
Ethan Yao <ey709@nyu.edu>
2020-03-14 18:39:25 -0400

    LL2
    added code for lander and mountains and text font

--- P3/Project 3/main.cpp
+++ P3/Project 3/main.cpp
@@ -10,6 +10,7 @@
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+#include <vector>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -17,22 +18,54 @@
 ShaderProgram program;
 glm::mat4 viewMatrix, modelMatrix, projectionMatrix, terrainMatrix, leftMatrix;
 
+GLuint font;
+
 SDL_Window* displayWindow;
 bool gameIsRunning = true;
 
 float rightPosY = 0.0f;
 float leftPosY = 0.0f;
 
-float ballPosX = 0.0f;
-float ballPosY = 0.0f;
+float landerPosX = 0.0f;
+float landerPosY = 3.0f;
 
-float ballSpeedX = 0.05f;
-float ballSpeedY = 0.05f;
+float landerSpeedX = 0.0005f;
+float landerSpeedY = 0.0005f;
 
+float landerAccelX = 0.0f;
+float landerAccelY = 0.0f;
+
+float ballRotateAngle = 0.0f;
+
+GLuint LoadTexture(const char* filePath)
+{
+	int w, h, n;
+	unsigned char* image = stbi_load(filePath, &w, &h, &n, STBI_rgb_alpha);
+
+	if (image == NULL)
+	{
+		std::cout << "Unable to load image. Make sure the path is correct\n";
+		assert(false);
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_REPEAT);
+
+	stbi_image_free(image);
+	return textureID;
+}
 
 void Initialize() {
 	SDL_Init(SDL_INIT_VIDEO);
-	displayWindow = SDL_CreateWindow("Pong!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	displayWindow = SDL_CreateWindow("Lunar Lander!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 
@@ -57,11 +90,70 @@ void Initialize() {
 	program.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	GLuint font = LoadTexture("font.png");
 }
 
 
 void Shutdown() {
 	SDL_Quit();
+}
+
+
+void DrawText(ShaderProgram* program, GLuint fontTextureID, std::string text, float size, float spacing, glm::vec3 position)
+{
+	float width = 1.0f / 16.0f;
+	float height = 1.0f / 16.0f;
+
+	std::vector<float> vertices;
+	std::vector<float> texCoords;
+
+	for (int i = 0; i < text.size(); i++)
+	{
+		int index = (int)text[i];
+		float offset = (size + spacing) * i;
+
+		float u = (float)(index % 16) / 16.0f;
+		float v = (float)(index / 16) / 16.0f;
+
+		vertices.insert(vertices.end(),
+			{
+				offset + (-0.5f * size), 0.5f * size,
+				offset + (-0.5f * size), -0.5f * size,
+				offset + (0.5f * size), 0.5f * size,
+				offset + (0.5f * size), -0.5f * size,
+				offset + (0.5f * size), 0.5f * size,
+				offset + (-0.5f * size), -0.5f * size
+			});
+
+		texCoords.insert(texCoords.end(),
+			{
+				u, v,
+				u, v + height,
+				u + width, v,
+				u + width, v + height,
+				u + width, v,
+				u, v + height
+			});
+	}
+
+	glm::mat4 modelMatrix = glm::mat4(1.0f);
+	modelMatrix = glm::translate(modelMatrix, position);
+	program->SetModelMatrix(modelMatrix);
+
+	glUseProgram(program->programID);
+	
+	glVertexAttribPointer(program->positionAttribute, 2, GL_FLOAT, false, 0, vertices.data());
+	glEnableVertexAttribArray(program->positionAttribute);
+
+	glVertexAttribPointer(program->texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords.data());
+	glEnableVertexAttribArray(program->texCoordAttribute);
+
+	glBindTexture(GL_TEXTURE_2D, fontTextureID);
+	glDrawArrays(GL_TRIANGLES, 0, (int)(text.size() * 6));
+
+	glDisableVertexAttribArray(program->positionAttribute);
+	glDisableVertexAttribArray(program->texCoordAttribute);
 }
 
 int main(int argc, char* argv[]) {
@@ -79,6 +171,8 @@ int main(int argc, char* argv[]) {
 		//Update();
 		//Render();
 
+		if (landerAccelX != 0) landerAccelX -= 0.0000001;
+
 		glClear(GL_COLOR_BUFFER_BIT);
 		program.SetProjectionMatrix(projectionMatrix);
 		program.SetViewMatrix(viewMatrix);
@@ -87,97 +181,108 @@ int main(int argc, char* argv[]) {
 		// Keyboard Controller
 		const Uint8* keys = SDL_GetKeyboardState(NULL);
 
-		if (keys[SDL_SCANCODE_W] && leftPosY <= 3.25f) {
-			leftPosY += 0.1f;
+		if (keys[SDL_SCANCODE_LEFT]) {
+			landerAccelX += 0.0000005f;
+			ballRotateAngle -= 0.25f;
 		}
-		if (keys[SDL_SCANCODE_S] && leftPosY >= -3.25f) {
-			leftPosY -= 0.1f;
-		}
-		if (keys[SDL_SCANCODE_UP] && rightPosY <= 3.25f) {
-			rightPosY += 0.1f;
-		}
-		if (keys[SDL_SCANCODE_DOWN] && rightPosY >= -3.25f) {
-			rightPosY -= 0.1f;
+		if (keys[SDL_SCANCODE_RIGHT]) {
+			landerAccelX -= 0.0000005f;
+			ballRotateAngle += 0.25f;
 		}
 
 
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-		// Right Bar Controller
+		// Terrain
 		terrainMatrix = glm::mat4(1.0f);
 		terrainMatrix = glm::translate(terrainMatrix, glm::vec3(0.0f, rightPosY, 0.0f));
 		program.SetModelMatrix(terrainMatrix);
 
-		float right[] = { 4.0f, 0.0f, 3.9f, 1.0f, 4.0f, 1.0f, 3.9f, 1.0f, 4.0f, 0.0f, 3.9f, 0.0f };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, right);
+		float terrain[] = { -4.5f, -4.0f, 
+							-2.5f, -1.0f,
+
+							-2.5f, -1.0f,
+							-0.5f, -3.0f,
+
+							-0.5f, -3.0f,
+							0.0f, -3.0f,
+
+							0.0f, -3.0f,
+							0.0f, -2.0f,
+
+							0.0f, -2.0f,  // TARGET LINES
+							1.0f, -2.0f,
+				
+							1.0f, -2.0f, 
+							2.0f, -3.0f,
+
+							2.0f, -3.0f,
+							3.0f, -1.0f,
+
+							3.0f, -1.0f,
+							3.0f, 1.5f,
+
+							3.0f, 1.5f,
+							4.5f, 1.5f
+						};
+
+
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, terrain);
 		glEnableVertexAttribArray(program.positionAttribute);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_LINES, 0, 18);
 		glDisableVertexAttribArray(program.positionAttribute);
 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-		// Left Bar controller
-		leftMatrix = glm::mat4(1.0f);
-		leftMatrix = glm::translate(leftMatrix, glm::vec3(0.0f, leftPosY, 0.0f));
-		program.SetModelMatrix(leftMatrix);
-
-		float left[] = { -4.0f, -1.0f, -3.9f, 0.0f, -4.0f, 0.0f, -3.9f, 0.0f, -4.0f, -1.0f, -3.9f, -1.0f };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, left);
-		glEnableVertexAttribArray(program.positionAttribute);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
-		glDisableVertexAttribArray(program.positionAttribute);
-
-
 		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		// Controls for Ball
+		// Controls for Lander
 
-		if (ballPosY <= -3.6f || ballPosY >= 3.6f)
-		{
-			ballSpeedY = -ballSpeedY;
-		}
-		
-		if ((ballPosX >= 3.8f))
-		{
-			if ((ballPosY >= rightPosY + 1.0f) || (ballPosY <= rightPosY - 0.75f))
-			{
-				ballSpeedX = 0.0f;
-				ballSpeedY = 0.0f;
-			}
-			else
-			{
-				ballSpeedX = -ballSpeedX;
-			}
-		}
-		else if (ballPosX <= -3.8f)
-		{
-			if ((ballPosY >= leftPosY + 1.0f) || (ballPosY <= leftPosY - 0.75f))
-			{
-				ballSpeedX = 0.0f;
-				ballSpeedY = 0.0f;
-			}
-			else
-			{
-				ballSpeedX = -ballSpeedX;
-			}
-		}
 
-		ballPosX += ballSpeedX;
-		ballPosY += ballSpeedY;
+		landerSpeedX += landerAccelX;
+		landerSpeedY += landerAccelY;
+
+
+		landerPosX += landerSpeedX;
+		landerPosY -= landerSpeedY;
 
 		modelMatrix = glm::mat4(1.0f);
-		modelMatrix = glm::translate(modelMatrix, glm::vec3(ballPosX, ballPosY, 0.0f));
+		modelMatrix = glm::translate(modelMatrix, glm::vec3(landerPosX, landerPosY, 0.0f)); 
+
+		modelMatrix = glm::rotate(modelMatrix, 
+										glm::radians(ballRotateAngle),
+										glm::vec3(0.0f, 0.0f, 1.0f));
+
 		program.SetModelMatrix(modelMatrix);
 
-		float ball[] = { -0.1f, -0.1f, 0.1f, 0.1f, -0.1f, 0.1f, 0.1f, 0.1f, -0.1f, -0.1f, 0.1f, -0.1f };
-		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ball);
+		float lander[] = { 
+							-0.15f, -0.15f, 
+							-0.05f, 0.15f, 
+							-0.05f, -0.15f, 
+							
+							0.05f, 0.15f, 
+							0.15f, -0.15f, 
+							0.05f, -0.15f,
+
+							-0.05f, -0.15f,
+							-0.05f, 0.15f,
+							0.05f, 0.15f,
+
+							0.05f, 0.15f,
+							-0.05f, -0.15f,
+							0.05f, -0.15f
+		};
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, lander);
 		glEnableVertexAttribArray(program.positionAttribute);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 12);
 
 		glDisableVertexAttribArray(program.positionAttribute);
+
+		//GLuint font = LoadTexture("textSheet.png");
+		DrawText(&program, font, "Victory", 0.1f, 0.1f, glm::vec3(1.0f, 1.0f, 0.0f));
 
 		SDL_GL_SwapWindow(displayWindow);
 	}
